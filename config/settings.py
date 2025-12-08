a01ec8bb5df69f3c4397dc28156962804518c1e7
suzuki-hayato-pila <suzuki.hayato.jellyware@gmail.com>
2025-12-08 20:49:32 +0900

    postgresql設定完了

--- config/settings.py
+++ config/settings.py
@@ -74,10 +74,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'private_diary',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres1234',  # あなたが設定したパスワード
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
